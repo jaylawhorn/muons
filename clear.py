fd963f69713715b3e230821bb812fd84d47c37f4
seltinge <seltinge@mit.edu>
2014-04-07 14:48:54 -0400

    Update clear.py

--- clear.py
+++ clear.py
@@ -4,5 +4,6 @@ import visa
 """ Program to return control to the scope keyboard """
 
 test = visa.instrument("USB0::0x1AB1::0x0488::DS1BC130900036::INSTR")
+test.write(":RUN")
 test.write(":KEY:LOCK DISABLE")
 test.close()
